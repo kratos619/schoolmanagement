2a6af8fb0a237eecd4329329f34fe9bea0851726
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-18 19:31:44 +0530

    Add Courses Properties

--- database/migrations/2018_12_18_135410_create_courses_table.php
+++ database/migrations/2018_12_18_135410_create_courses_table.php
@@ -15,6 +15,8 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
+            $table->name('name');
+            $table->integer('years')->default(3);
             $table->timestamps();
         });
     }
