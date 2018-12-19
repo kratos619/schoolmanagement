c25ccdd2404c63b12369c5132b70508440b7742d
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-19 10:42:00 +0530

    Course CRUD

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Courses;
 
 class CoursesController extends Controller
 {
@@ -13,18 +14,10 @@ class CoursesController extends Controller
      */
     public function index()
     {
-        //
+        $all_courses = Courses::all();
+        return response()->json($all_courses, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -34,7 +27,17 @@ class CoursesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name'=> 'required',
+            'years' => 'required'
+        ]);
+        
+        Courses::create([
+            'name' => $request->name,
+            'years' => $request->years
+        ]);
+
+        return response()->json("data added Successfully", 200);
     }
 
     /**
@@ -45,19 +48,10 @@ class CoursesController extends Controller
      */
     public function show($id)
     {
-        //
+        $selected_course = Courses::findeOrFail($id);
+        return response()->json($selected_course, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -68,7 +62,14 @@ class CoursesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $selected_course = Courses::findOrFail($id);
+        $this->validate($request, [
+            'name' => 'required',
+            'years' => 'required'
+        ]);
+
+        $selected_course->update($request->all());
+        return response()->json("Success", 200);
     }
 
     /**
@@ -79,6 +80,8 @@ class CoursesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $select_course = Courses::findOrFail($id);
+        $select_course->delete();
+        return response()->json("success", 200);
     }
 }
